ed2546b89e240cc414df3c345d5bfac466cfe2c0
Maliktalha123 <talhanoormalik0321@gmail.com>
2024-05-11 22:31:58 +0500

    Few work in project

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,37 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
+import "./Weather.css";
 
 function Weather() {
-  return (
-    <div>
-      <div classNameName="container">
+
+const [weather,setWeather] =useState({
+    humidity : "20",
+    temp:"37",
+    city:"paris",
+    speed:"120"
+})
+    return (
+    <div className="container my-5">
+      <div
+        className="mx-auto text-center border rounded text-white p-4 "
+        style={{ width: "400px", background: "#3B5FAB" }}
+      >
         <h1 style={{ color: "red", backgroundColor: "black" }}>
           Weather Forecast
         </h1>
 
-        <div className="mb-3 row">
-          <label for="staticEmail" className="col-sm-2 col-form-label">
-            Email
-          </label>
-          <div className="col-sm-10">
-            <input
-              type="text"
-              readonly
-              className="form-control-plaintext"
-              id="staticEmail"
-              value="email@example.com"
-            />
+        <form className="d-flex my-3">
+          <input
+            className="form-control me-2"
+            name="city"
+            type="search"
+            placeholder="City"
+          />
+          <button className="btn btn-outline-light" type="submit">
+            Search
+          </button>
+        </form>
+        <h1 className="my-3">
+          <i class="bi bi-cloud-drizzle-fill my-5"></i>
+        </h1>
+        <h1 className="my-2">{weather.temp}</h1>
+        <h1 className="my-2">{weather.city}</h1>
+        <div className="lastLine d-flex jc-sb justify-content-between my-5">
+          <div
+            className="humidity text-center "
+            style={{ color: "white", }}
+          >
+            <i class="bi bi-water"></i> Humidity <br />
+            {weather.humidity}
           </div>
-        </div>
-        <div className="mb-3 row">
-          <label for="inputPassword" className="col-sm-2 col-form-label">
-            Password
-          </label>
-          <div className="col-sm-10">
-            <input
-              type="password"
-              className="form-control"
-              id="inputPassword"
-            />
+          <div
+            className="windSpeed text-center"
+            style={{ color: "white", }}
+          >
+            <i class="bi bi-lightning-fill"></i> Wind Speed
+            <br />
+            {weather.speed}
           </div>
         </div>
       </div>
